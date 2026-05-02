d2ec1d19858dd30342301a62ba6e0a2faa47ef4b
Gilbert Ramirez <gram@alumni.rice.edu>
2026-05-02 17:31:07 -0500

    Fill in missing code
    Add tests, but they don't pass yet

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
-
+#
+# This checks if if the Formatter.PrettyPrinter has any compilation problems.
+# If so, it stops. If that module compiles, then it builds the entire Gren
+# front-end to the compiler.
+#
 COMPILER_DIR=$(realpath ../compiler)
 export GREN_BIN="$COMPILER_DIR"/gren
 
